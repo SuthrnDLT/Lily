2d0ab09e65cdc789fd269223362837d0bf2e6e27
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-18 17:51:10 -0500

    Suppressed CA1848 because high perf logging is not needed right now.

--- src/Lily/Worker.cs
+++ src/Lily/Worker.cs
@@ -1,20 +1,27 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Lily;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
+    private readonly ILogger _logger;
 
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
     }
 
+    [SuppressMessage(
+        "Microsoft.Security",
+        "CA1848:AvoidUseOfRuntimeUnverifiableCode",
+        Justification = "High performance logging is not needed right now.")]
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            _logger.Log(LogLevel.Information, $"Worker running at: {DateTimeOffset.Now}");
+            await Task.Delay(millisecondsDelay: 1000, stoppingToken)
+                      .ConfigureAwait(continueOnCapturedContext: false);
         }
     }
 }
