25bcf3896c3d277f3c99e5c5ee25f0744f24caca
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-18 16:41:41 -0500

    Created service project.

---
+++ src/Lily/Program.cs
@@ -0,0 +1,10 @@
+using lily;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
