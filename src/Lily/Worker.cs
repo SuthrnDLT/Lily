274d51c613b9e20746eb9c649bb04f1adcbba876
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-18 19:15:58 -0500

    Removed demo while loop from primary worker.

--- src/Lily/Worker.cs
+++ src/Lily/Worker.cs
@@ -15,14 +15,9 @@ public class Worker : BackgroundService
         "Microsoft.Security",
         "CA1848:AvoidUseOfRuntimeUnverifiableCode",
         Justification = "High performance logging is not needed right now.")]
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.Log(LogLevel.Information, $"Starting Lily.");
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.Log(LogLevel.Information, $"Lily is running.");
-            await Task.Delay(millisecondsDelay: 1000, stoppingToken)
-                      .ConfigureAwait(continueOnCapturedContext: false);
-        }
+        return Task.CompletedTask;
     }
 }
