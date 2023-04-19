1f6e891e4a1b722391e4ed1683246b321a1bc51c
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-18 19:56:35 -0500

    Updated dependencies.

--- src/Lily/Program.cs
+++ src/Lily/Program.cs
@@ -1,12 +1,17 @@
 // Disable 1852 on the basis that sealing Program is not a concern for this project.
 #pragma warning disable CA1852
 
+using Discord.Commands;
+using Discord.WebSocket;
 using Lily;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        services.AddHostedService<Worker>()
+                .AddSingleton<SecretService>()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>();
     })
     .Build();
 
