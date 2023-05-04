5f0cdfa866b2734c3da35528cd210d6dd683765f
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-05-03 23:01:30 -0500

    Created identify command.

---
+++ src/Lily/Commands/IdentifyCommand.cs
@@ -0,0 +1,18 @@
+using Lily.Core.Commands;
+
+namespace Lily.Commands;
+
+internal sealed class IdentifyCommand : Command
+{
+    public IdentifyCommand(ILogger<IdentifyCommand> logger) :
+        base(logger)
+    { }
+
+    [Command("identify")]
+    public async Task Identify()
+    {
+        Logger.LogInformation("Received invocation for the identify command.");
+        string message = "👋 Hey there! I'm Lily, a simple but ambitious chatbot created by tacosontitan.";
+        await ReplyAsync(message);
+    }
+}
