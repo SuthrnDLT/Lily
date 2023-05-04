173d7e2d307a329e75c6ffbb5dd93d1161c4c057
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-05-03 21:27:54 -0500

    Simplified the service setup.

--- src/Lily/Program.cs
+++ src/Lily/Program.cs
@@ -1,17 +1,12 @@
-// Disable 1852 on the basis that sealing Program is not a concern for this project.
-#pragma warning disable CA1852
+#pragma warning disable CA1852 // Disable 1852 on the basis that sealing Program is not a concern for this project.
 
-using Discord.Commands;
-using Discord.WebSocket;
 using Lily;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>()
-                .AddSingleton<SecretService>()
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton<CommandService>();
+                .AddDiscord();
     })
     .Build();
 
