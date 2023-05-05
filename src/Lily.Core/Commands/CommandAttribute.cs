19be5158264bcfc3f32fb981efa3f44b3ade5d9f
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-05-05 14:29:22 -0500

    Updated command attribute to derive from slash command.

--- src/Lily.Core/Commands/CommandAttribute.cs
+++ src/Lily.Core/Commands/CommandAttribute.cs
@@ -1,17 +1,17 @@
-using DiscordCommandAttribute = Discord.Commands.CommandAttribute;
+using Discord.Interactions;
 
 namespace Lily.Core.Commands;
 
 /// <summary>
 /// Represents an <see cref="Attribute"/> for marking <see cref="Command"/>s.
 /// </summary>
-public sealed class CommandAttribute : DiscordCommandAttribute
+public sealed class CommandAttribute : SlashCommandAttribute
 {
     /// <summary>
     /// Creates a new <see cref="CommandAttribute"/> instance.
     /// </summary>
     /// <param name="key">Gets or sets the key for invoking the <see cref="Command"/>.</param>
-    public CommandAttribute(string name) :
-        base(name)
+    public CommandAttribute(string name, string description) :
+        base(name, description, ignoreGroupNames: false, RunMode.Async)
     { }
 }
